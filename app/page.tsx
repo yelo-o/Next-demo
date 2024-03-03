254ec394e5740f91db66a4d18e7df4a1a8106bd4
yelo-o <flyordig@gmail.com>
2024-03-03 10:07:00 +0900

    make landing page with shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -5,36 +7,60 @@ import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts'
 import Image from 'next/image';
 
+import { Button } from "@/components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import LoginForm from './ui/login-form';
+
+
 export default function Page() {
+  const formSchema = z.object({
+    username: z.string().min(2).max(50),
+  })
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-gray-900 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className={styles.shape} />
+          {/* <div className={styles.shape} /> */}
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Bookkeeper.</strong> <br />
+            please sign in to use this website. <br/>
+            Or you can go to demo page.
           </p>
           <Link
-            href="/login"
+            href="/demo"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Demo Page</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
+          {/* shadcn design */}
+          {/* <Input type='username' placeholder='username'/>
+          <Input type='password' placeholder='password'/>
+
+          <Button asChild>
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link href="/signup">SignUp</Link>
+          </Button> */}
+
+          {/* nextjs design */}
+          <LoginForm />
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center justify-center p-6 md:w-4/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image src="/hero-desktop.png" width={1000} height={760} className="hidden md:block"
+          <Image src="/book-img.png" width={1000} height={760} className="hidden md:block"
                 alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/book-img.png"
             width={560}
             height={620}
             className="block md:hidden"
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -5,14 +5,15 @@ import {
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+// import { Button } from './button';
+import { Button } from "@/components/ui/button"
 
 export default function LoginForm() {
   return (
     <form className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+      <div className="flex-1 rounded-lg bg-gray-50">
         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
+          {/* Please log in to continue. */}
         </h1>
         <div className="w-full">
           <div>
@@ -20,7 +21,7 @@ export default function LoginForm() {
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
               htmlFor="email"
             >
-              Email
+              username
             </label>
             <div className="relative">
               <input
@@ -28,7 +29,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="Enter your username"
                 required
               />
               <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -56,6 +57,7 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
+        <SignUpButton />
         <div className="flex h-8 items-end space-x-1">
           {/* Add form errors here */}
         </div>
@@ -70,4 +72,11 @@ function LoginButton() {
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
     </Button>
   );
+}
+function SignUpButton() {
+  return (
+    <Button className="mt-4 w-full" variant="outline">
+      Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-900" />
+    </Button>
+  )
 }
